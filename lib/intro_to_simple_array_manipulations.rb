348261774684ad56001778bb10540da71c11cb95
Chris Bojemski <chris.bojemski@gmail.com>
2018-08-22 20:42:24 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -9,3 +9,24 @@ end
 def using_pop(a) 
   a.pop 
 end 
+
+def pop_with_args(a)
+  a.pop(2) 
+end 
+
+def using_shift(a) 
+  a.shift 
+end 
+
+def shift_with_args(a)
+  a.shift 2 
+end 
+
+def using_concat(a, b) 
+  a.concat(b)
+end 
+
+def using_insert(a, e)
+  a.insert(e, 3)
+end 
+
